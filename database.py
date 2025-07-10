0ad8555c24321f94c7b2301c20deb664d455d5ae
bld237 <muforbelmond20@gmail.com>
2025-07-10 20:13:12 +0100

    updated connections

--- database.py
+++ database.py
@@ -3,8 +3,15 @@ from sqlalchemy.orm import sessionmaker
 from models import Base
 
 SQLALCHEMY_DATABASE_URL = "mysql+pymysql://u150585579_taskplus:ONLYmore!112@srv1847.hstgr.io/u150585579_taskplus_db"
-
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    pool_pre_ping=True,          
+    pool_recycle=280,             
+    pool_timeout=30,               
+    connect_args={
+        "connect_timeout": 10       
+    }
+)
 SessionLocal = sessionmaker(autoflush=False, autocommit=False, bind=engine)
 
 Base.metadata.create_all(bind=engine)
